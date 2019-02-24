a1f76615eedc7628c45f0fc88ea4086efdbb8a4f
mike <mikelynn@rams.colostate.edu>
2019-02-23 18:16:42 -0700

    Removed folder from repository

--- TF-IDF-master/jobA/runhadoop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export HADOOP_CLASSPATH=/usr/lib/jvm/java-openjdk/lib/tools.jar
-$HADOOP_HOME/bin/hdfs dfs -rm -r /testLoc/out*
-$HADOOP_HOME/bin/hadoop jar unigram.jar unigram.UnigramDriver /testLoc/cs435data /testLoc/output
--- TF-IDF-master/jobB/runhadoop.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export HADOOP_CLASSPATH=/usr/lib/jvm/java-openjdk/lib/tools.jar
-$HADOOP_HOME/bin/hadoop jar unigram.jar unigram.UnigramDriver /testLoc/PA2Dataset /testLoc/output
